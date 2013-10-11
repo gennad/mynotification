76151e6fe5903efe6303630df2fd35f89114d37b
Gennadiy Zlobin <gennad@dreamindustries.co>
2013-10-12 03:08:10 +0400

    init

---
+++ src/com/gennad/mynotification/CreateNotificationActivity.java
@@ -0,0 +1,47 @@
+package com.gennad.mynotification;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateNotificationActivity extends Activity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+	  
+    super.onCreate(savedInstanceState);
+    
+    setContentView(R.layout.activity_main);
+  }
+
+  public void createNotification(View view) {
+    // Prepare intent which is triggered if the
+    // notification is selected
+    Intent intent = new Intent(this, NotificationReceiverActivity.class);
+    PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+    // Build notification
+    // Actions are just fake
+    Notification noti = new Notification.Builder(this)
+        .setContentTitle("New mail from " + "test@gmail.com")
+        .setContentText("Subject").setSmallIcon(R.drawable.ic_launcher)
+        .setContentIntent(pIntent)
+        .addAction(R.drawable.ic_launcher, "Call", pIntent)
+        .addAction(R.drawable.ic_launcher, "More", pIntent)
+        .addAction(R.drawable.ic_launcher, "And more", pIntent).build();
+    
+    NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    
+    // hide the notification after its selected
+    noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+    notificationManager.notify(0, noti);
+  }
+} 
